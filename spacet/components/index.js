f63e4cde8a136c9f9d8989336eb901c30c80caf0
Anton kaaj Holmgren <anton.holmgren3@hotmail.com>
2023-05-03 13:42:43 +0200

    Update

--- spacet/components/index.js
+++ spacet/components/index.js
@@ -1,16 +1,8 @@
 import Navbar from './Navbar';
 import { TypingText, TitleText } from './CustomTexts';
-import StartSteps from './StartSteps';
-import StartTech from './StartTech';
-import StartFeature from './StartFeature';
-import Navtech from './Navtech';
 
 export {
     Navbar,
     TitleText,
     TypingText,
-    StartSteps,
-    StartTech,
-    StartFeature,
-    Navtech,
 }
