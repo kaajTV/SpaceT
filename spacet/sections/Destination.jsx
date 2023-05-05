2f1285ca2abb4c32699305f4d16c64ed54f53133
Anton kaaj Holmgren <anton.holmgren3@hotmail.com>
2023-05-05 13:23:06 +0200

    Update

--- spacet/sections/Destination.jsx
+++ spacet/sections/Destination.jsx
@@ -19,7 +19,7 @@ const DestinationSection = () => {
     }
 
     return (
-        <section className="xl:mt-[175px] md:mt-[140px] mt-[120px] z-0">
+        <section className="xl:mt-[175px] md:mt-[140px] mt-[120px] w-screen min-h-screen z-0">
             <motion.div
                 variants={staggerContainer}
                 initial="hidden"
